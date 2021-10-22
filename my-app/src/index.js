52a9e4149191a00b9828ae77fce013d9509fbbbf
Sara Thomas <thom1461@algonquinlive.com>
2021-10-22 13:10:28 -0400

    React Deployment

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter hashType="hashbang">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
